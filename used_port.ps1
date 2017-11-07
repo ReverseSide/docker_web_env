21b099d87839922fd409270534b2321fd21067f7
Vincent Montet Win10 Work <vincentmontet.vm@gmail.com>
2017-11-07 13:09:40 +0100

    Add script for used port

---
+++ used_port.ps1
@@ -0,0 +1,29 @@
+﻿[System.Collections.ArrayList]$array_port_used = @{}
+
+Function Get-ListeningTCPConnections {            
+[cmdletbinding()]            
+param(            
+)            
+            
+try {            
+    $TCPProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()            
+    $Connections = $TCPProperties.GetActiveTcpListeners()            
+    foreach($Connection in $Connections) {            
+        if($Connection.address.AddressFamily -eq "InterNetwork" ) { $IPType = "IPv4" } else { $IPType = "IPv6" }            
+                    
+        $OutputObj = New-Object -TypeName PSobject            
+        $OutputObj | Add-Member -MemberType NoteProperty -Name "LocalAddress" -Value $connection.Address            
+        $OutputObj | Add-Member -MemberType NoteProperty -Name "ListeningPort" -Value $Connection.Port            
+        $OutputObj | Add-Member -MemberType NoteProperty -Name "IPV4Or6" -Value $IPType            
+        $OutputObj   
+        
+        $array_port_used.Add($Connection.Port)  
+    }            
+            
+} catch {            
+    Write-Error "Failed to get listening connections. $_"            
+}  
+
+return $array_port_used
+         
+}
