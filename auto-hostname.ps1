99cd68e56da7e74bb9065e69715b100f18f5d6a5
Vincent Montet Win10 Work <vincentmontet.vm@gmail.com>
2017-11-08 10:51:59 +0100

    Manage .env for port

--- auto-hostname.ps1
+++ auto-hostname.ps1
@@ -1,53 +1,64 @@
-﻿$linebelow_ports=$false
+﻿Import-module .\check_port.ps1
 
+$linebelow_ports=$false
 $array = @{}
+[System.Collections.ArrayList]$container_name_array = @{}
+[int]$port_start=8080
 
 #Create var by reading .env file
-
 Get-Content .env | Foreach-Object{
    $var = $_.Split('=')
-   New-Variable -Name $var[0] -Value $var[1]
+   #If var exist (re)set the value 
+   if (Get-Variable -Name $var[0] -ErrorAction SilentlyContinue){
+    Set-Variable -Name $var[0] -Value $var[1]
+   }
+   #Else create the var
+   else{
+    New-Variable -Name $var[0] -Value $var[1]
+   }
 }
 
-Write-Host $Project_Name
 
+#Count Nomber of Container
+$FileContent = Get-Content "docker-compose.yml"
+$Matches = Select-String -InputObject $FileContent -Pattern "container_name :" -AllMatches
+$Nb_Container = $Matches.Matches.Count
+
+#Request Free Port
+$array_port = (Get-FreePorts $port_start $Nb_Container)
+
+#Parse the YML
 Get-Content docker-compose.yml | ForEach-Object -Begin {} -Process {} -End {}{
 
+    #If port seems to be on the second line
     if ($linebelow_ports -eq $true){
-    
-        
         if($_ -like '*-*:*'){
-
+            #String manipulation for extract port
             $container_ports = $_.Substring($_.IndexOf('"')+1)
             $container_ports = $container_ports.Substring(0, $container_ports.IndexOf(":"))
-
         }
-
         $linebelow_ports = $false
-      
     }
-    
-    
+
+    #If line containe the name of container  
     if ($_ -like '*container_name*'){
-
-         $container_name = $_.Substring($_.IndexOf(":")+2)
-         $container_name = $container_name.Substring(0, $container_name.IndexOf("_"))
-         $container_name = $Project_Name+"."+$container_name
+         #Recontruct the name of container with .env var
+         $container_name = $_.replace('${Project_Name}',$Project_Name)
+         $container_name = $container_name.Substring($_.IndexOf(':')+1)
+         $container_name_array.Add($container_name) | Out-Null
+         $port_container = $array_port[$container_name_array.Count -1 ]
+         
+         Add-Content .env $container_name"_port="$port_container
     }
-
+    #If line containe ports of container
     if ($_ -like '*ports*') {
-
         if ($_ -like '*-*:*'){
-
             $container_ports = $_.Substring($_.IndexOf('"')+1)
             $container_ports = $container_ports.Substring(0, $container_ports.IndexOf(":"))
-
-
         }
         else {
-
+            #If port is not on the same line
             $linebelow_ports = $true
-   
         }       
     }
 
@@ -73,6 +84,3 @@ Get-Content docker-compose.yml | ForEach-Object -Begin {} -Process {} -End {}{
     Add-Content .ergo $ergoadd
 
  }
-
- $command = '"ergo run"'
-iex "& $command"
